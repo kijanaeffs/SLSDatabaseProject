a67bdd37afc1561a42a1e7ffc16fe0253c14dbb2
kijanaeffs <k.effs95@gmail.com>
2017-04-22 19:58:23 -0400

    Added controller for adding a new POI location

---
+++ SLSDatabaseProject/src/controller/AddPOILocationController.java
@@ -0,0 +1,54 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Created by Kijana on 4/22/2017.
+ */
+public class AddPOILocationController {
+    @FXML
+    TextField locationField;
+
+    @FXML
+    ComboBox cityBox;
+
+    @FXML
+    ComboBox stateBox;
+
+    @FXML
+    TextField zipField;
+
+    @FXML
+    Button backButton;
+
+    @FXML
+    Button submitButton;
+
+
+    @FXML
+    private void handleBackPressed() throws IOException {
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass()
+                .getResource("../view/AddDataPointScreen.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    @FXML
+    private void handleSubmitPressed() throws IOException {
+        Stage stage = (Stage) submitButton.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass()
+                .getResource("../view/AddDataPointScreen.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
