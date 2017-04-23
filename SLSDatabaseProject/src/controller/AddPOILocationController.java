a4a8a24d915bd4c14a1de8f8b81953ebe54e3ace
MilesJ150 <miles.johnson1996@gmail.com>
2017-04-23 01:04:15 -0400

    Added queries to register page

--- SLSDatabaseProject/src/controller/AddPOILocationController.java
+++ SLSDatabaseProject/src/controller/AddPOILocationController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import fxapp.MainFXApplication;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,6 +13,12 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Kijana on 4/22/2017.
@@ -33,6 +42,26 @@ public class AddPOILocationController {
     @FXML
     private Button submitButton;
 
+    @FXML
+    private void initialize() throws SQLException {
+        Connection conn = MainFXApplication.getConnection();
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT DISTINCT State FROM CITYSTATE ORDER BY State");
+        List<String> stateList = new ArrayList<String>();
+        while(rs.next()) {
+            stateList.add(rs.getString("State"));
+        }
+        ObservableList states = FXCollections.observableList(stateList);
+        stateBox.setItems(states);
+
+        ResultSet rs2 = statement.executeQuery("SELECT DISTINCT City FROM CITYSTATE ORDER BY City");
+        List<String> cityList = new ArrayList<String>();
+        while(rs2.next()) {
+            cityList.add(rs2.getString("City"));
+        }
+        ObservableList cities = FXCollections.observableList(cityList);
+        cityBox.setItems(cities);
+    }
 
     @FXML
     private void handleBackPressed() throws IOException {
--- SLSDatabaseProject/src/controller/RegisterController.java
+++ SLSDatabaseProject/src/controller/RegisterController.java
@@ -1,14 +1,23 @@
 package controller;
 
 
+import fxapp.MainFXApplication;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
 
 public class RegisterController {
 
@@ -18,9 +27,76 @@ public class RegisterController {
     @FXML
     private Button backButton;
 
+    @FXML
+    private ComboBox cityCombo;
 
+    @FXML
+    private ComboBox stateCombo;
 
+    @FXML
+    private ComboBox typeCombo;
 
+    private Connection conn = MainFXApplication.getConnection();
+    @FXML
+    private void initialize() throws SQLException {
+
+        String queryU = "SELECT COLUMN_TYPE FROM information_schema.COLUMNS " +
+                "WHERE TABLE_NAME = ? AND COLUMN_NAME = ? ";
+        PreparedStatement preparedStmtU = conn.prepareStatement(queryU);
+        preparedStmtU.setString(1, "USERS");
+        preparedStmtU.setString(2, "UserType");
+        ResultSet rsu = preparedStmtU.executeQuery();
+        rsu.next();
+        String result = rsu.getString(1);
+        int resultLength = result.length();
+        result = result.substring(6,resultLength - 2);
+        List<String> userTypeList = new ArrayList<>();
+        String delims = "','";
+        String[] tokens = result.split(delims);
+        for (int i = 0; i < tokens.length; i++ ) {
+            userTypeList.add(tokens[i]);
+        }
+        ObservableList userTypes = FXCollections.observableList(userTypeList);
+        typeCombo.setItems(userTypes);
+
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT DISTINCT State FROM CITYSTATE ORDER BY State");
+        List<String> stateList = new ArrayList<String>();
+        while(rs.next()) {
+            stateList.add(rs.getString("State"));
+        }
+        ObservableList states = FXCollections.observableList(stateList);
+        stateCombo.setItems(states);
+    }
+
+    @FXML
+    private void handleStatePicked() throws SQLException {
+        String selState = stateCombo.getValue().toString();
+        String query2 = "SELECT DISTINCT City FROM CITYSTATE WHERE State = ? ORDER BY City";
+        PreparedStatement preparedStmt = conn.prepareStatement(query2);
+        preparedStmt.setString(1, selState);
+        ResultSet rs2 = preparedStmt.executeQuery();
+        List<String> cityList = new ArrayList<String>();
+        while(rs2.next()) {
+            cityList.add(rs2.getString("City"));
+        }
+        ObservableList cities = FXCollections.observableList(cityList);
+        cityCombo.setItems(cities);
+    }
+
+    @FXML
+    private void handleCityClicked() throws IOException {
+        Object selState = stateCombo.getValue();
+        //error message does not work yet
+        if (selState == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("No state selected");
+            alert.setHeaderText("Please select a state.");
+            alert.setContentText("The state must be selected before the city.");
+            alert.showAndWait();
+            return;
+        }
+    }
 
     @FXML
     private void handleCreatePressed() throws IOException {
